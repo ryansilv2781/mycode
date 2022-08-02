22eb2067a1f8f3b4c8c80514932d8fbf7fb6f6ce
ryan silva <ryansilv2781@users.noreply.github.com>
2022-08-02 14:26:54 +0000

    reason for commit

---
+++ tuesdaychallenge_02aug.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# tuesday challenge on 08/02/2022
+# input asking the user's name
+name = input ("Please Enter Your Name: ")
+
+# input asking what day of the week it is
+day = input ("Please Enter The Day of The Week: ")
+
+# print statement that reads "Hello, <name>! Happy <day>!
+print ("Hello, " + name " ! Happy " + day "!")
+

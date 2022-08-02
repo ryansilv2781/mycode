3a7625950386942db4af63bd8cd7868f34310234
ryan silva <ryansilv2781@users.noreply.github.com>
2022-08-02 14:47:58 +0000

    reason for commit

--- tuesdaychallenge_02aug.py
+++ tuesdaychallenge_02aug.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python3
 
 # tuesday challenge on 08/02/2022
+# my attempt:
+
 # input asking the user's name
-name = input ("Please Enter Your Name: ")
+# name = input ("Please Enter Your Name: ")
 
 # input asking what day of the week it is
-day = input ("Please Enter The Day of The Week: ")
+# day = input ("Please Enter The Day of The Week: ")
 
 # print statement that reads "Hello, <name>! Happy <day>!
-print ("Hello, " + name " ! Happy " + day "!")
+# print ("Hello, " + name "! Happy " + day "!")
 
+
+
+# answer
+name = input("What is your name?")
+day = input("what is the day today?")
+
+# print("Hello "+name+"! Happy "+day+"!") 
+# this one is even cleaner!
+print(f"Hello {name}! Happy {day}!"

19fe05125eafef64c8c0380a01b88347144a053d
Ryan Silva <ryansilva@users.noreply.github.com>
2022-08-11 08:06:25 +0000

    Simpson API fun

---
+++ challenge1/iss-challenge.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Returning the location of the ISS in latitude/longitude"""
+import requests
+    
+URL= "http://api.open-notify.org/iss-now.json"
+def main():
+    resp= requests.get(URL).json()
+
+# GET request to get Lon: and Lat:
+    lon= resp["iss_position"]["longitude"] 
+    lat= resp["iss_position"]["latitude"]
+
+    print(f"""
+    CURRENT LOCATION OF THE ISS:
+    Lon: {lon}
+    Lat: {lat}
+    """)
+
+if __name__ == "__main__":
+    main()
---
+++ simpsonsquotes/simpsonsquotes.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+"""Quotes from The Simpsons!"""
+
+import urllib.request
+import json
+import requests
+
+# defining the url
+SIM= "https://thesimpsonsquoteapi.glitch.me/quotes"
+SIMCH= "https://thesimpsonsquoteapi.glitch.me/quotes?character="
+def main():
+    simpsonquote = urllib.request.urlopen(SIM)
+    simpson = simpsonquote.read()
+    simquote = json.loads(simpson.decode('utf-8'))
+    
+    # showed index (this was '0')
+    # for idx, word in enumerate(simquote):
+    #    print(idx, word)
+
+    # getting random quote or by character        
+    # print(simquote)
+    print('-------------------------')
+    print("THE SIMPSONS QUOTE: \n")
+    print('\"'+simquote[0]['quote']+'\"')
+    print('         -',simquote[0]['character']) 
+    print('-------------------------')
+    
+    input() # user can press 'enter'
+    choice= input("Do want a \'random\' qoute or would you rather search for one \'by name\'?\n").lower().title()
+    print('-------------------------') 
+    while choice == 'Random':
+        main()
+
+    if choice == 'By Name':             # .lower().title() converts 'bY nAmE' >> 'By Name' 
+        usrsel= input("Who would you like to hear a quote from?\n").lower().title()
+        print('-------------------------')
+        selquote = requests.get(SIMCH + usrsel).json()
+        # print(f"{resp}")                                  testing..................
+        # sim1 = urllib.request.urlopen(SIMCH + usr_select) testing..................
+        # simselect = sim1.read()                           testing..................
+        # selquote = json.loads(simselect.decode('utf-8'))  testing..................
+        print("HERE'S THE THE QUOTE YOU ASKED FOR: \n")
+        print('\"'+selquote[0]['quote']+'\"')
+        print('         -',selquote[0]['character'])
+        print('-------------------------')
+
+    else:
+        answer= input("Did you mean \'random\'?\n")
+
+        while answer == 'y':
+            main()
+        else:
+            print("ADIOS!")
+            print('------------------------')
+            exit() 
+
+if __name__ == "__main__":
+    main()   
